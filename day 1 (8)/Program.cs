8faeea25ba47779a53989868c1b089fecca97e5a
Нисик <@DESKTOP-ALORV4C>
2023-04-17 14:26:23 +0300

    Добавьте файлы проекта.

---
+++ day 1 (8)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 10; // здесь можно задать значение N
+        int k = 1;
+        int sum = 0;
+        while (sum + k <= n)
+        {
+            sum += k;
+            k++;
+        }
+        k--;
+
+        Console.WriteLine("Наибольшее число K: " + k);
+        Console.WriteLine("Сумма чисел от 1 до K: " + sum);
+
+        Console.ReadKey();
+    }
+}
